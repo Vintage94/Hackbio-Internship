0279df968ed4692da187de99a0d6c42c8efb690b
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-03-28 08:51:15 +0100

    Update script.sh

--- Stage 2/script.sh
+++ Stage 2/script.sh
@@ -6,6 +6,10 @@ wget "https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz?download=1
 # Download the reverse sequence
 wget "https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz?download=1"
 
+# Rename the files
+mv ERR8774458_1.fastq.gz?download=1 ERR8774458_1.fastq.gz
+mv ERR8774458_2.fastq.gz?download=1 ERR8774458_2.fastq.gz
+
 # Quality control
 
 
