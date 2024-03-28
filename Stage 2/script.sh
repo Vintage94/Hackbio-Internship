28f995f9914912d1f8fc887551083e812cfa5938
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-03-28 09:31:43 +0100

    Update script.sh

--- Stage 2/script.sh
+++ Stage 2/script.sh
@@ -10,8 +10,14 @@ wget "https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz?download=1
 mv ERR8774458_1.fastq.gz?download=1 ERR8774458_1.fastq.gz
 mv ERR8774458_2.fastq.gz?download=1 ERR8774458_2.fastq.gz
 
-# Quality control
+# Make directory to output the results of quality control
+mkdir QC_Reports
 
+# Quality control
+fastqc ERR8774458_1.fastq.gz ERR8774458_2.fastq.gz -o QC_Reports
+
+#Use MultiQC to summarize the QC results
+.multiqc
 
 # Trimming
 
